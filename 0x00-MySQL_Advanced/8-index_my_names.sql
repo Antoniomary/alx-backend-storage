d564b0f1edab1119412f77514df247a2bd27aae5
Antoniomary <amanso.totti@yahoo.com>
2024-08-23 17:00:45 +0000

    8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,4 @@
 -- creates an index idx_name_first on the table names
 -- and the first letter of name.
 
-ALTER TABLE names DROP INDEX IF EXISTS idx_name_first;
-
 CREATE INDEX idx_name_first ON names(name(1));
