edac7fcdfb5ee31bda04b36c9c94d1252f299c4d
Antoniomary <amanso.totti@yahoo.com>
2024-08-28 01:21:18 +0000

    10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""contains the function update_topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    mongo_collection.update_one(
+            {'name': name},
+            {'$set': {'topic': topics}}
+    )        
