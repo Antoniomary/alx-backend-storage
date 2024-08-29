4b2add005e932de96f4fc1bcb4f43cbdf50bebb8
Antoniomary <amanso.totti@yahoo.com>
2024-08-29 04:54:08 +0000

    fixed: self.redis to self._redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,7 +10,7 @@ class Cache:
     def __init__(self):
         """constructor function"""
         self._redis = redis.Redis()
-        self.redis.flushdb()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate a random key (e.g. using uuid).
