fb7e89f5c6fb7eeae7f7269dcf6fda42e7370166
Antoniomary <amanso.totti@yahoo.com>
2024-08-30 21:15:44 +0000

    web.py

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""contains Cache class"""
+import redis
+import requests
+
+
+
+def get_page(url: str) -> str:
+    """uses the requests module to obtain the HTML content of a particular URL
+       and returns it.
+    """
+    response = requests.get(url)
+    if response.ok:
+        key = f"count:{url}"
+        cache = redis.Redis()
+        cache.setex(key, 10, response.text)
+    return response.text
